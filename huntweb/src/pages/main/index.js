fa438dfe319103b87540635810a8d88ec3621466
Wallace <wbescrovaine@gmail.com>
2020-04-07 11:32:55 -0300

    Listando produtos

---
+++ huntweb/src/pages/main/index.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import api from "../../services/api";
+
+import "./styles.css";
+
+export default class Main extends Component {
+    state = {
+        products: [],
+    }
+
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get("/products")
+
+        this.setState({products: response.data.docs});
+    };
+
+    render() {
+        const {products} = this.state;
+
+        return (
+        <div className="product-list">
+            {products.map(product => (
+                <article key={product._id}>{product.title}
+                    <strong> {product.title} </strong>
+                    <p>{product.description}</p>
+                    <a href="">Acessar</a>
+                </article>
+
+
+            ))}
+        </div>
+
+    )
+    }
+}
