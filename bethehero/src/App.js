d29925b48610316a71cb67197b2bec080ed665a5
Wallace <wbescrovaine@gmail.com>
2020-05-05 10:18:20 -0300

    First commit Be the Hero

---
+++ bethehero/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+import Header from './Header';
+
+
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div>
+      <Header >Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
+
+  );
+}
+
+export default App;
