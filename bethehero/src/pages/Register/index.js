c5615bdc94703a249db0d91c6c1f28052aae89b3
Wallace <wbescrovaine@gmail.com>
2020-05-11 16:02:09 -0300

    New Incidente

--- bethehero/src/pages/Register/index.js
+++ bethehero/src/pages/Register/index.js
@@ -34,6 +34,5 @@ export default function Register() {
                 </form>
             </div>
         </div>
-
     );
 }
--- bethehero/src/routes.js
+++ bethehero/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 
 export default function Routes() {
@@ -14,6 +15,8 @@ export default function Routes() {
                 <Route path="/register" component={Register} />
 
                 <Route path="/profile" component={Profile} />
+
+                <Route path="/incidents/new" component={NewIncident} />
             </Switch>
         </BrowserRouter>
     );
