3373617f979edcced2bf373cbac2fadda85deb67
Wallace <wbescrovaine@gmail.com>
2020-05-12 11:30:54 -0300

    Login

--- bethehero/src/pages/Logon/index.js
+++ bethehero/src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -9,15 +11,39 @@ import logoImg from '../../assets/logo.svg'
 
 
 export default function Logon() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        try {
+            const res = await api.post('session', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', res.data.name);
+
+            history.push('/profile');
+
+        } catch (err) {
+            alert('falha no login, tente novamente.')
+        }
+    }
+
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be the Hero" />
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu Logon</h1>
 
-                    <input placeholder="Sua ID" />
+                    <input
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
                     <button type="submit" className="button"> Entrar </button>
                     <Link className="back-link" to="/register">
                         <FiLogIn size={13} color="#E02041" />
