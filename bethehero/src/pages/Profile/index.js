647cea7bbeafba378bfcd62a44eba77afa95cbee
Wallace <wbescrovaine@gmail.com>
2020-05-18 10:57:23 -0300

    logout da Sessão

--- bethehero/src/pages/Profile/index.js
+++ bethehero/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -11,6 +11,7 @@ import logoImg from '../../assets/logo.svg';
 export default function Profile() {
     const [incidents, setIncidents] = useState([]);
 
+    const history = useHistory();
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
 
@@ -39,7 +40,11 @@ export default function Profile() {
             alert('Erro ao deletar caso, tente novamente.');
 
         }
+    }
 
+    function handleLogout() {
+        localStorage.clear();
+        history.push('/');
     }
 
     return (
@@ -49,7 +54,7 @@ export default function Profile() {
                 <span>Bem vinda {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
